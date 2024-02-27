9cd574780c6337696a73d09cd2ba62a86a241c23
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2024-02-27 16:55:36 +0530

    Time: 58 ms (99.80%) | Memory: 60.2 MB (30.50%) - LeetSync

---
+++ 2804-compact-object/compact-object.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    if (obj === null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj !== "object") return obj;
+
+    // This for loop deals with when obj is an iterable object
+    // Steps 4-5 as described above
+    const compacted = {};
+    for (const key in obj) {
+        let value = compactObject(obj[key]);
+        if (value) compacted[key] = value;
+    }
+
+    return compacted;
+};
